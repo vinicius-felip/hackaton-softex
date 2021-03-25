3bddb3de7b516409298be28ee905ee6ecc0fbcb5
viniciusFelipeS <viniciusfelipe.xd@gmail.com>
2021-03-24 21:14:10 -0300

    add: autoload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '3075f86350fda3f33cd73d84d4fb8a95b3946604',
+    'name' => 'hackaton/softex',
+  ),
+  'versions' => 
+  array (
+    'hackaton/softex' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3075f86350fda3f33cd73d84d4fb8a95b3946604',
+    ),
+  ),
+);
