c00ff13b1a75efe3c0c73de59f030056fb45d855
viniciusFelipeS <viniciusfelipe.xd@gmail.com>
2021-03-26 15:28:41 -0300

    add: Página Sobre

---
+++ app/Controller/Pages/About.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Pages;
+
+use App\Http\Response;
+use App\Utils\View;
+
+
+class About extends Page{
+    
+    /**
+     * Método responsável por retornar o conteudo da /About
+     *  
+     * @return string
+     */
+    public static function getAbout(){
+      //VIEW DA About
+      
+      $content = View::render('pages/sobre',[
+        "name"        => 'SOBRE',
+        "description" => ''
+      ]);
+
+
+      //RETORNA A VIEW DA PAGINA
+      return new Response(200, parent::getPage('TITULO', $content));
+    }
+}
