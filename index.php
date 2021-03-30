07d5aca4ebfbaa669dcab64936e7bdceb9d35632
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2021-03-30 15:05:17 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require __DIR__ . "/vendor/autoload.php";
 use App\Http\Router;
 
 
-define('URL', 'http://localhost/php%20poo/hackaton-softex');
+define('URL', 'http://localhost/hackaton-softex\index.php');
 
 $obRouter = new Router(URL);
 
