9a042789bb66fc2540c2f038b3412d6e9ffb6bc7
viniciusFelipeS <viniciusfelipe.xd@gmail.com>
2021-03-31 14:21:58 -0300

    fixed: Comment

--- app/Controller/Auth/Auth.php
+++ app/Controller/Auth/Auth.php
@@ -7,10 +7,26 @@ use App\Utils\View;
 use Exception;
 
 class Auth
-{
+{    
+    /**
+     * Instância de Reqquest
+     *
+     * @var Request
+     */
     private $request;
+        
+    /**
+     * Parâmetros de GET
+     *
+     * @var array
+     */
     private $params = [];
-
+    
+    /**
+     * Método responsável por realizar a conexão com o app do facebook
+     *
+     * @return Facebook
+     */
     private static function getFacebook()
     {
         return new \League\OAuth2\Client\Provider\Facebook([
@@ -20,34 +36,62 @@ class Auth
             "graphApiVersion" => getenv('FACEBOOK_graphApiVersion')
         ]);
     }
-
-    public static function sair(){
+    
+    /**
+     * Método responsável por desautenticar o usuário
+     *
+     */
+    public static function sair()
+    {
         session_destroy();
-        header('location: '.getenv('URL_BASE'));
+        header('location: ' . getenv('URL_BASE'));
     }
-
+    
+    /**
+     * Método responsável por receber a url de autenticação
+     *
+     * @return void
+     */
     public static function getAuthUrl()
     {
         return self::getFacebook()->getAuthorizationUrl([
             "scope" => "email"
         ]);
     }
-
+    
+    /**
+     * Método responsável por retornar a /home
+     *
+     * @param  Request $request
+     */
     public static function getHome($request)
     {
         return Home::getHome($request);
     }
-
+    
+    /**
+     * Método responsável por definir a Request para verificação do objeto
+     *
+     * @param  Request $request
+     */
     public function setRequest($request)
     {
         $this->request = $request;
     }
-
+    
+    /**
+     * Método responsável por definir os parâmetros de autenticação
+     *
+     */
     public function setParams()
     {
         $this->params = !is_null($this->request) ? $this->request->getQueryParams() : [];
     }
-
+    
+    /**
+     * Método responsável por saber se o houve a tentativa de autenticação
+     *
+     */
     public function islogged()
     {
         if (!is_null($this->request)) {
@@ -57,7 +101,12 @@ class Auth
             }
         }
     }
-
+    
+    /**
+     * Método responsável por saber retornar o usuario para o erro
+     *
+     * @return string
+     */
     public function hasError()
     {
         if (isset($this->params['error'])) {
@@ -66,18 +115,35 @@ class Auth
         }
         return '';
     }
-
+    
+    /**
+     * Método responsável por saber se o usuario conseguiu se autenticar
+     *
+     */
     public function hasCode()
     {
         if (isset($this->params['code']) && empty($_SESSION['userLogged'])) {
             try {
                 $token = self::getFacebook()->getAccessToken("authorization_code", ["code" => $this->params['code']]);
                 $_SESSION['userLogged'] = self::getFacebook()->getResourceOwner($token);
-                header('location: '.getenv('URL_BASE'));
+                header('location: ' . getenv('URL_BASE'));
                 exit;
             } catch (Exception $e) {
                 die($e->getMessage());
             }
         }
     }
-}   
+    
+    /**
+     * Método responsável por impedir usuario de acessar areas restritas
+     *
+     * @return void
+     */
+    public static function logged()
+    {
+        if (!isset($_SESSION['userLogged'])) {
+            header('location: ' . URL_BASE);
+            exit;
+        }
+    }
+}
